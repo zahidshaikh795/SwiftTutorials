df4f9a81ac0ffe41e2187c115720c90cdd3b6d70
zahidshaikh <zahidshaikh795@gmail.com>
2017-03-20 13:29:41 +0530

    Initial Commit

---
+++ SwiftTutorials/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTutorials
+//
+//  Created by Zahid Shaikh on 20/03/17.
+//  Copyright © 2017 Zahid Shaikh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
