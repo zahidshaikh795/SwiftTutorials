3ca3400aeaa114fbfadc6636a90e70783a583798
zahidshaikh <zahidshaikh795@gmail.com>
2017-03-20 23:14:20 +0530

    initial commit

---
+++ SwiftTutorials/MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Constants and Variables
+
+var str = "Hello, playground"
+
+let iu = 90
+
+print(iu)
+print(str)
+
+
+
+
+
+
