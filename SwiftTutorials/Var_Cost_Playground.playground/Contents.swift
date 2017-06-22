764fbe5d73a5b7c55762efa79bdcf3592bab48bb
zahidshaikh <zahidshaikh795@gmail.com>
2017-06-22 16:47:36 +0530

    Variable and constant done

---
+++ SwiftTutorials/Var_Cost_Playground.playground/Contents.swift
@@ -0,0 +1,78 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+
+// Arithmetic Operations
+
+2+6   // OK
+2 + 6 // OK
+//  2 +6  // ERROR
+//  2+ 6  // ERROR
+22/7
+22.0/7.0
+28%10
+1<<3 // shift the digit left by 3
+100>>2 // shift the digit right by 2
+sin(90.0*Double.pi/180)
+cos(135.0*Double.pi/180)
+max(-12, 23)
+
+// Constant and Variable
+
+let number=min(sqrt(64.0), 12)
+//number=8   // ERROR you cant assign to value: number is a let constant
+var varNum:Double = 35
+varNum = 34.87
+varNum -= 5
+print(varNum)
+var varNum2 = 50
+varNum2 *= 2
+varNum2 /= 2
+
+let myAge:Int64 = 25
+print(myAge)
+
+
+var averageAge:Double = 25
+averageAge = (25+30)/2
+let testNum = 24
+let evanOdd = testNum%2
+var answer = 0
+answer+=1
+answer+=10
+answer*=10
+answer>>3
+
+let exercises = 11
+var solvedExercises = 0
+solvedExercises+=1
+
+let a:Int = 46
+let b:Int = 10
+// 1
+let answer1: Int = (a * 100) + b
+// 2
+//let answer2: Int = (a * 100) + (b * 100)
+// 3
+//let answer3:Int = (a * 100) + (b / 10)
+solvedExercises+=1
+   (5 * 3) - (4 / 2) * 2
+solvedExercises+=1
+let farenhiet:Double = 100
+var celcius:Double = farenhiet - 32
+celcius = celcius/1.8
+solvedExercises+=1
+36/8
+
+let divident:Double = 28
+let divisor:Double = 5
+
+
+
+
+
+
+
